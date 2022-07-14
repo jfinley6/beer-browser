e35fa6d596258d6a659c7a1c63b4cf751ed06740
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-07-14 10:35:57 -0600

    toggle button

--- app.js
+++ app.js
@@ -471,6 +471,10 @@ function changeHeaderColor(e) {
 
 }
 
+function changeLightDark(e) {
+    console.log('hello')
+}
+
 
 // document.querySelector("#settings").addEventListener('click', () => {
 //     localStorage.setItem('color', 'yellow')
